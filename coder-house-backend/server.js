7c8d1679f5824d0d42b1204eeabf54e6ae353a90
Aman Singh <aman.iit96@gmail.com>
2023-06-21 10:28:14 +0530

    comment

--- coder-house-backend/server.js
+++ coder-house-backend/server.js
@@ -1,4 +1,4 @@
-// require('dotenv').config();
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
@@ -10,16 +10,18 @@ const cookieParser = require('cookie-parser');
 const ACTIONS = require('./actions');
 
 const io = require('socket.io')(server, {
-    cors: {
-        origin: process.env.FRONT_URL,
-        methods: ['GET', 'POST'],
-    },
+  cors: {
+    origin: process.env.FRONT_URL,
+    methods: ['GET', 'POST'],
+  },
 });
 
 app.use(cookieParser());
 const corsOption = {
-    credentials: true,
-    origin: [process.env.FRONT_URL],
+  credentials: true,
+  // origin: [process.env.FRONT_URL],
+  origin: 'http://localhost:3000',
+  optionSuccessStatus: 200,
 };
 app.use(cors(corsOption));
 app.use('/storage', express.static('storage'));
@@ -30,104 +32,102 @@ app.use(express.json({ limit: '8mb' }));
 app.use(router);
 
 app.get('/', (req, res) => {
-    res.send('Hello from express Js');
+  res.send('Hello from express Js');
 });
 
 // Sockets
 const socketUserMap = {};
 
 io.on('connection', (socket) => {
-    console.log('New connection', socket.id);
-    socket.on(ACTIONS.JOIN, ({ roomId, user }) => {
-        socketUserMap[socket.id] = user;
-        const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
-        clients.forEach((clientId) => {
-            io.to(clientId).emit(ACTIONS.ADD_PEER, {
-                peerId: socket.id,
-                createOffer: false,
-                user,
-            });
-            socket.emit(ACTIONS.ADD_PEER, {
-                peerId: clientId,
-                createOffer: true,
-                user: socketUserMap[clientId],
-            });
-        });
-        socket.join(roomId);
+  console.log('New connection', socket.id);
+  socket.on(ACTIONS.JOIN, ({ roomId, user }) => {
+    socketUserMap[socket.id] = user;
+    const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
+    clients.forEach((clientId) => {
+      io.to(clientId).emit(ACTIONS.ADD_PEER, {
+        peerId: socket.id,
+        createOffer: false,
+        user,
+      });
+      socket.emit(ACTIONS.ADD_PEER, {
+        peerId: clientId,
+        createOffer: true,
+        user: socketUserMap[clientId],
+      });
     });
+    socket.join(roomId);
+  });
 
-    socket.on(ACTIONS.RELAY_ICE, ({ peerId, icecandidate }) => {
-        io.to(peerId).emit(ACTIONS.ICE_CANDIDATE, {
-            peerId: socket.id,
-            icecandidate,
-        });
+  socket.on(ACTIONS.RELAY_ICE, ({ peerId, icecandidate }) => {
+    io.to(peerId).emit(ACTIONS.ICE_CANDIDATE, {
+      peerId: socket.id,
+      icecandidate,
     });
+  });
 
-    socket.on(ACTIONS.RELAY_SDP, ({ peerId, sessionDescription }) => {
-        io.to(peerId).emit(ACTIONS.SESSION_DESCRIPTION, {
-            peerId: socket.id,
-            sessionDescription,
-        });
+  socket.on(ACTIONS.RELAY_SDP, ({ peerId, sessionDescription }) => {
+    io.to(peerId).emit(ACTIONS.SESSION_DESCRIPTION, {
+      peerId: socket.id,
+      sessionDescription,
     });
+  });
 
-    socket.on(ACTIONS.MUTE, ({ roomId, userId }) => {
-        const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
-        clients.forEach((clientId) => {
-            io.to(clientId).emit(ACTIONS.MUTE, {
-                peerId: socket.id,
-                userId,
-            });
-        });
+  socket.on(ACTIONS.MUTE, ({ roomId, userId }) => {
+    const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
+    clients.forEach((clientId) => {
+      io.to(clientId).emit(ACTIONS.MUTE, {
+        peerId: socket.id,
+        userId,
+      });
     });
+  });
 
-    socket.on(ACTIONS.UNMUTE, ({ roomId, userId }) => {
-        const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
-        clients.forEach((clientId) => {
-            io.to(clientId).emit(ACTIONS.UNMUTE, {
-                peerId: socket.id,
-                userId,
-            });
-        });
+  socket.on(ACTIONS.UNMUTE, ({ roomId, userId }) => {
+    const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
+    clients.forEach((clientId) => {
+      io.to(clientId).emit(ACTIONS.UNMUTE, {
+        peerId: socket.id,
+        userId,
+      });
     });
+  });
 
-    socket.on(ACTIONS.MUTE_INFO, ({ userId, roomId, isMute }) => {
-        const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
-        clients.forEach((clientId) => {
-            if (clientId !== socket.id) {
-                console.log('mute info');
-                io.to(clientId).emit(ACTIONS.MUTE_INFO, {
-                    userId,
-                    isMute,
-                });
-            }
+  socket.on(ACTIONS.MUTE_INFO, ({ userId, roomId, isMute }) => {
+    const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
+    clients.forEach((clientId) => {
+      if (clientId !== socket.id) {
+        console.log('mute info');
+        io.to(clientId).emit(ACTIONS.MUTE_INFO, {
+          userId,
+          isMute,
         });
+      }
     });
+  });
 
-    const leaveRoom = () => {
-        const { rooms } = socket;
-        Array.from(rooms).forEach((roomId) => {
-            const clients = Array.from(
-                io.sockets.adapter.rooms.get(roomId) || []
-            );
-            clients.forEach((clientId) => {
-                io.to(clientId).emit(ACTIONS.REMOVE_PEER, {
-                    peerId: socket.id,
-                    userId: socketUserMap[socket.id]?.id,
-                });
-
-                // socket.emit(ACTIONS.REMOVE_PEER, {
-                //     peerId: clientId,
-                //     userId: socketUserMap[clientId]?.id,
-                // });
-            });
-            socket.leave(roomId);
+  const leaveRoom = () => {
+    const { rooms } = socket;
+    Array.from(rooms).forEach((roomId) => {
+      const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
+      clients.forEach((clientId) => {
+        io.to(clientId).emit(ACTIONS.REMOVE_PEER, {
+          peerId: socket.id,
+          userId: socketUserMap[socket.id]?.id,
         });
-        delete socketUserMap[socket.id];
-    };
 
-    socket.on(ACTIONS.LEAVE, leaveRoom);
+        // socket.emit(ACTIONS.REMOVE_PEER, {
+        //     peerId: clientId,
+        //     userId: socketUserMap[clientId]?.id,
+        // });
+      });
+      socket.leave(roomId);
+    });
+    delete socketUserMap[socket.id];
+  };
 
-    socket.on('disconnecting', leaveRoom);
+  socket.on(ACTIONS.LEAVE, leaveRoom);
+
+  socket.on('disconnecting', leaveRoom);
 });
 
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
--- coder-house-frontend/src/http/index.js
+++ coder-house-frontend/src/http/index.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
+const aman = process.env.REACT_APP_API_URL;
+console.log('aman:', aman);
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
-    withCredentials: true,
-    headers: {
-        'Content-type': 'application/json',
-        Accept: 'application/json',
-    },
+  baseURL: process.env.REACT_APP_API_URL,
+  withCredentials: true,
+  headers: {
+    'Content-type': 'application/json',
+    Accept: 'application/json',
+  },
 });
 
 // List of all the endpoints
@@ -18,34 +20,34 @@ export const createRoom = (data) => api.post('/api/rooms', data);
 export const getAllRooms = () => api.get('/api/rooms');
 export const getRoom = (roomId) => api.get(`/api/rooms/${roomId}`);
 
-// Interceptors
+// Interceptors;
 api.interceptors.response.use(
-    (config) => {
-        return config;
-    },
-    async (error) => {
-        const originalRequest = error.config;
-        if (
-            error.response.status === 401 &&
-            originalRequest &&
-            !originalRequest._isRetry
-        ) {
-            originalRequest.isRetry = true;
-            try {
-                await axios.get(
-                    `${process.env.REACT_APP_API_URL}/api/refresh`,
-                    {
-                        withCredentials: true,
-                    }
-                );
+  (config) => {
+    console.log('config:', config);
+    return config;
+  },
+  async (error) => {
+    console.log('error:aman', error.response);
+    const originalRequest = error.config;
+    console.log('originalRequest:', originalRequest);
+    if (
+      error.response.status === 401 &&
+      originalRequest &&
+      !originalRequest._isRetry
+    ) {
+      originalRequest.isRetry = true;
+      try {
+        await axios.get(`${process.env.REACT_APP_API_URL}/api/refresh`, {
+          withCredentials: true,
+        });
 
-                return api.request(originalRequest);
-            } catch (err) {
-                console.log(err.message);
-            }
-        }
-        throw error;
+        return api.request(originalRequest);
+      } catch (err) {
+        console.log(err.message);
+      }
     }
+    throw error;
+  }
 );
 
 export default api;
